086da76e099ffad1b83e97bf4d6253a8f7fb63f8
Travis Kohlbeck <me+git@travisk.info>
2020-01-10 09:50:30 -0500

    gets forms working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function submitCredentials() {
-  
-}
 
 function App() {
+  let [ userText, setUserText ] = React.useState('')
+  let [ passText, setPassText ] = React.useState('')
+
+  function submitCredentials(e) {
+    e.preventDefault()
+    alert(userText)
+    alert(passText)
+  }  
 
   return (
-    <div id="app-root">
-      <form onSubmit={submitCredentials}>
-        <input></input>
+    <div id='app-root'>
+      <form id='credentials-form' onSubmit={submitCredentials}>
+        <p>Smartmarks</p>
+        <input
+          id='user-input'
+          onChange={e => setUserText(e.target.value)}
+          value={userText}
+        />
+        <br />
+        <input
+          id='pass-input'
+          onChange={e => setPassText(e.target.value)}
+          value={passText}
+        />
+        <br />
+        <button>
+          Submit
+        </button>
       </form>
     </div>
   );
