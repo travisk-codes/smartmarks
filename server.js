a7230d6cadc8102f65b10a885649d88adec807a9
Travis Kohlbeck <me+git@travisk.info>
2020-01-10 15:49:23 -0500

    queries db, responds with json

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const body_parser = require('body-parser')
-//const mysql = require('mysql')
+const mysql = require('mysql')
+const cors = require('cors')
 const server = express()
+const { mysql_secrets } = require('./secrets')
 
+const db = mysql.createConnection(mysql_secrets)
+
+server.use(cors())
 server.use(body_parser.json())
 server.get('/', (req, res) => {
-	return res.send('hello from server!')
+	let user_query = 'select * from `bookmarks` where user = "' + req.query.user_id + '"'
+	db.query(user_query, (err, result) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send(err)
+		}
+		if (!result.length) {
+			return res.send('{"msg":"This user has no bookmarks"}')
+		}
+		console.log(result)
+		return res.send(result)
+	})
+})
+server.post('/', (req, res) => {
+	const bookmark = req.body.bookmark
+	const username = req.body.username
+	// mysql look up if user exists by map reducing users in bm list
+	// send 404 if user not found?
+	// otherwise send list of bookmarks
+	// mysql by itering through bookmarks where user is username
+	// map each one to a object, add to a list
+	// respond with json string of list of objects
+
 })
 
 const port = 7779
--- src/App.js
+++ src/App.js
@@ -8,39 +8,43 @@ const cipher = salt => {
   const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code)    
 
   return text => text.split('')
-      .map(textToChars)
-      .map(applySaltToChar)
-      .map(byteHex)
-      .join('')
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join('')
 }
 const decipher = salt => {
   const textToChars = text => text.split('').map(c => c.charCodeAt(0))
   const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code)
 
   return encoded => encoded.match(/.{1,2}/g)
-      .map(hex => parseInt(hex, 16))
-      .map(applySaltToChar)
-      .map(charCode => String.fromCharCode(charCode))
-      .join('')
+    .map(hex => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map(charCode => String.fromCharCode(charCode))
+    .join('')
 }
-
 
 function App() {
   let [ userText, setUserText ] = React.useState('')
   let [ passText, setPassText ] = React.useState('')
 
-  async function submitCredentials(e) {
+  function submitCredentials(e) {
     e.preventDefault()
     const password_cipher = cipher(passText)
-    const data = { username: password_cipher(userText)}
-    const response = await fetch('https://api.travisk.info/bookmarks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-    return await response.json()
+    const user_id = password_cipher(userText)
+    const params = { user_id }
+    let url = new URL('https://travisk.info/smartmarks')
+    Object.keys(params)
+      .forEach(key => url.searchParams.append(key, params[key]))
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(json => {
+        console.log(json)
+      })
+      .catch(e => console.log(e))
+ 
   }  
 
   return (
