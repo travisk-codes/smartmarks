27f876bd251faf75538c7c966573f35e238c3931
Travis Kohlbeck <me+git@travisk.info>
2020-03-11 17:50:40 -0400

    cleans up put and post endpoints

--- server.js
+++ server.js
@@ -45,55 +45,18 @@ router.get('/tags', (req, res) => {
 	})
 })
 
-function tagsDbEntriesToString(a, b, c) {
-	return `("${a}", "${b}", "${c}")`
-}
-
 router.post('/bookmarks', (req, res) => {
 	let { uid, user, title, url, tags } = req.body
-
-	/* 	let sql = [
-		'insert into bookmarks SET',
-		' uid=?',
-		',user=?',
-		',title=?',
-		',url=?',
-		';',
-	]
-	if (tags) {
-		sql.concat(tags.map(tag => (
-			[
-				'insert into tags SET',
-				' uid=?',
-			]
-		)))
-	}
- */
-	let bookmark_query =
-		'insert into `bookmarks` (uid, user, title, url) values ("' +
-		uid +
-		'", "' +
-		user +
-		'", "' +
-		title +
-		'", "' +
-		url +
-		'")'
-	let tag_query
+	let query = `
+		insert into bookmarks (uid, user, title, url) values (?, ?, ?, ?);
+	`
 	if (tags.length) {
-		tag_query = 'insert into `tags` (uid, user, tag) values '
-		tags.forEach(t => {
-			tag_query += tagsDbEntriesToString(uid, user, t) + ','
-		})
-		tag_query = tag_query.replace(/.$/, ';')
-		bookmark_query += ';' + tag_query
+		query += 'insert into tags (uid, user, tag) values'
+		tags.forEach(tag => (query += `("${uid}", "${user}", "${tag}"),`))
+		query = query.replace(/.$/, ';')
 	}
-
-	db.query(bookmark_query, (err, results) => {
-		if (err) {
-			console.log(err)
-			return res.status(500).send(err)
-		}
+	db.query(query, [uid, user, title, url], (err, results) => {
+		if (err) return res.status(500).send(err)
 		return res.send(results)
 	})
 })
@@ -101,24 +64,17 @@ router.post('/bookmarks', (req, res) => {
 router.put('/bookmarks/:uid', (req, res) => {
 	let { url, tags, title, user } = req.body
 	let { uid } = req.params
-
 	let query = `
 		update bookmarks set title = ?, url = ? where uid = ?;
 		delete from tags where uid = ?;
 	`
 	if (tags.length) {
-		let addTagsQuery = 'insert into tags (uid, user, tag) values'
-		tags.forEach(tag => {
-			addTagsQuery += `("${uid}", "${user}", "${tag}"),`
-		})
-		addTagsQuery = addTagsQuery.replace(/.$/, ';')
-		query += addTagsQuery
+		query += 'insert into tags (uid, user, tag) values'
+		tags.forEach(tag => (query += `("${uid}", "${user}", "${tag}"),`))
+		query = query.replace(/.$/, ';')
 	}
 	db.query(query, [title, url, uid, uid], (err, result) => {
-		if (err) {
-			console.log(err)
-			return res.status(500).send(err)
-		}
+		if (err) return res.status(500).send(err)
 		return res.send(result)
 	})
 })
